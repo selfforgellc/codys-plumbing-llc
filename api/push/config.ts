91c6d232bb05e7f58ea1e81a423ae47ba5c54872
selfforgellc <selfforgellc@gmail.com>
2026-09-13 22:17:40 -0700

    Add files via upload

--- api/push/config.ts
+++ api/push/config.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { json, setCors } from "../_lib";
+import { json, setCors } from "../_lib.js";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   setCors(res);
--- api/push/subscribe.ts
+++ api/push/subscribe.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { adminRequired, json, readJson, setCors, supabaseAdmin } from "../_lib";
+import { adminRequired, json, readJson, setCors, supabaseAdmin } from "../_lib.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   setCors(res);
