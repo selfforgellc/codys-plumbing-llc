f2e7eecf48ff69541852130bda7979b6dd170df7
selfforgellc <selfforgellc@gmail.com>
2026-09-13 22:16:57 -0700

    Add files via upload

--- api/Admin/login.ts
+++ api/Admin/login.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { createAdminCookie, json, readJson, setCors } from "../_lib";
+import { createAdminCookie, json, readJson, setCors } from "../_lib.js";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   setCors(res);
